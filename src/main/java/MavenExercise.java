cb84848d964dec5497410fdcb598b0c6f122a12f
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-27 11:30:40 -0500

    maven exercises

--- src/main/java/MavenExercise.java
+++ src/main/java/MavenExercise.java
@@ -1,3 +1,5 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
 public class MavenExercise {
@@ -6,7 +8,21 @@ public class MavenExercise {
 
         Scanner sc = new Scanner(System.in);
 
+        // Prompt the user for input
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
 
+        // Check if the input is a number
+        boolean isNumber = StringUtils.isNumeric(input);
+        System.out.println("Is a number: " + isNumber);
+
+        // Flip the case of the input
+        String flippedCase = StringUtils.swapCase(input);
+        System.out.println("Flipped case: " + flippedCase);
+
+        // Reverse the input
+        String reversed = StringUtils.reverse(input);
+        System.out.println("Reversed string: " + reversed);
 
     }
 
